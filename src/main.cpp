c1f9126b1b934d9ca0872c8f719dda3626e15913
Bhartendu ji <bhartendu.ji_cs.aiml24@gla.ac.in>
2026-02-18 15:35:00 +0530

    Add OLED and DHT sensor support in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -35,3 +35,36 @@
  * 3. Calculate distance using speed of sound.
  * 4. Rotate servo based on threshold distance (15 cm).
  */
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+
+/** @brief OLED display width in pixels */
+#define SCREEN_WIDTH 128
+
+/** @brief OLED display height in pixels */
+#define SCREEN_HEIGHT 64
+
+/** @brief OLED reset pin (-1 if shared reset line is used) */
+#define OLED_RESET -1
+
+/**
+ * @brief SSD1306 OLED display object.
+ * 
+ * Uses I2C communication via the default Wire interface.
+ */
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+/** @brief Digital pin connected to DHT11 data line */
+#define DHTPIN 2
+
+/** @brief DHT sensor type definition */
+#define DHTTYPE DHT11
+
+/**
+ * @brief DHT sensor object instance.
+ */
+DHT dht(DHTPIN, DHTTYPE);
+
